000a1351a7e33f42116bf8fac59d841454362b7c
Joe Torraca <joe@joetorraca.com>
2015-09-26 03:15:17 -0400

    Implemented Twitter searching by geo-location. Still requires a search query unfortunately

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var randomstring = require('randomstring');
+
+var authSignature = require('oauth-signature');
+
 var request = require('request');
 
 var async = require('async');
@@ -29,7 +33,6 @@ router.get('/search/:LATITUDE/:LONGITUDE/', function(req, res, next) {
 				if (error) {
 					callback(error, null);
 				}
-				console.log(response)
 				if (body && response.statusCode == 200) {
 					var results = JSON.parse(body).data;
 
@@ -52,14 +55,47 @@ router.get('/search/:LATITUDE/:LONGITUDE/', function(req, res, next) {
 		},
 		twitter: function(callback)
 		{
-			callback(null, null);
+			var query_search_term = "";
+			var parameters = {
+				'oauth_consumer_key': config.get('twitter').consumer_key,
+				'oauth_nonce': randomstring.generate({charset: 'alphabetic'}),
+				'oauth_signature_method': 'HMAC-SHA1',
+				'oauth_timestamp': Math.floor(new Date() / 1000),
+				'oauth_token': config.get('twitter').oauth_token,
+				'oauth_version': '1.0',
+				'q': query_search_term,
+				'geocode': '"'+latitude+','+longitude+',2mi"'
+			};
+			var signature = authSignature.generate('GET',
+													config.get('twitter').FQDN + "search/tweets.json",
+													parameters,
+													config.get('twitter').consumer_secret,
+													config.get('twitter').oauth_token_secret);
+			var options = {
+				url: config.get('twitter').FQDN + 'search/tweets.json?q='+query_search_term+'&geocode="'+latitude+','+longitude+',2mi"',
+				headers: {
+					'Authorization': 'OAuth oauth_consumer_key="'+parameters['oauth_consumer_key']+'", oauth_nonce="'+parameters['oauth_nonce']+'", oauth_signature="'+signature+'", oauth_signature_method="'+parameters['oauth_signature_method']+'", oauth_timestamp="'+parameters['oauth_timestamp']+'", oauth_token="'+parameters['oauth_token']+'", oauth_version="'+parameters['oauth_version']+'"'
+				}
+			};
+			request(options, function(error, response, body)
+			{
+				if (error) {
+					callback(error, null);
+				}
+				if (body && response.statusCode == 200) {
+					callback(null, JSON.parse(body))
+				} else {
+					callback(true, body)
+				}
+			});
 		}
 	}, function(error, results)
 	{
 		if (error) {
 			res.send({'error': true, 'message': error})
+		} else {
+			res.send(results)
 		}
-		res.send(results)
 	});
 });
 
