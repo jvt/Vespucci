b29bf510da1efa34afa157001fc98f8d85453a24
Joe Torraca <joe@joetorraca.com>
2015-09-25 23:08:17 -0400

    Began work on pulling data from Instragram

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET api listing. */
-router.get('/search/:LOCATION_NAME', function(req, res, next) {
-  res.send('will return JSON');
+var ig = require('instagram-node').instagram();
+
+/* GET popular locations given a LATITUDE / LONGITUDE */
+router.get('/search/:LATITUDE/:LONGITUDE/', function(req, res, next) {
+	var latitude = req.params.LATITUDE;
+	var longitude = req.params.LONGITUDE;
+
+	if (!latitude || !longitude) {
+		res.send({ 'error': true, 'message': 'MISSING PARAMETER' });
+	}
+	
+	if (isNaN(latitude) || isNaN(longitude)) {
+		res.send({ 'error': true, 'message': 'PARAMETER NAN' });
+	}
+	res.send({'error': false});
 });
 
 module.exports = router;
