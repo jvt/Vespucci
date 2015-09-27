487008f497a5719a5fa1348aea6c40bd7397b28e
Joe Torraca <joe@joetorraca.com>
2015-09-26 22:38:35 -0400

    Commented out logger

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger(process.env.NODE_ENV));
+// app.use(logger(process.env.NODE_ENV));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
