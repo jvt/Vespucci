84bd222d367f7ea4b5534e0beed9a3c6637dd342
Joe Torraca <joe@joetorraca.com>
2015-09-28 07:16:26 -0400

    Fixed conditions that would sometimes make the map load slower than the rest of the file

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -241,8 +241,23 @@ function showArrays(event) {
 	infoWindow.open(map);
 }
 
-window.onload = function() {
+function loadJS(src, callback) {
+    var s = document.createElement('script');
+    s.src = src;
+    s.async = true;
+    s.onreadystatechange = s.onload = function() {
+        var state = s.readyState;
+        if (!callback.done && (!state || /loaded|complete/.test(state))) {
+            callback.done = true;
+            callback();
+        }
+    };
+    document.getElementsByTagName('head')[0].appendChild(s);
+}
+
+function init() {
 	initMap();
+	initAutocomplete();
 }
 
 function initAutocomplete() {
